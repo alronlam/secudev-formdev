a588c3983c8fe67e573df1cdb9792073f9700dea
Lafor8 <jmtw_84@yahoo.com>
2014-08-22 01:38:01 +0800

    Update autoacl_lang.php

--- application/language/english/autoacl_lang.php
+++ application/language/english/autoacl_lang.php
@@ -1,7 +1,6 @@
 <?php
 
-$lang['acl_view_salary_denied'] = "You do not have permission to view employer's salary!";
-$lang['acl_view_profile_denied'] = "You do not have permission to view other's profile!";
+$lang['acl_view_denied'] = "You do not have permission to view this page!";
 
 /* End of file autoacl_lang.php */
 /* Location: ./application/language/english/autoacl_lang.php */
